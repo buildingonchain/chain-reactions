f81a8c024c713f4cb25db8045ddff8fcedd5ddc5
Azhar Nurhussen <azharnurhussen@Azhars-MacBook-Pro.local>
2025-02-21 13:53:08 -0600

    Set up webpack and SDK bundling with proper initialization

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify"),
+      "fs": false
+    }
+  },
+  plugins: [
+    new Dotenv()
+  ]
+}; 
